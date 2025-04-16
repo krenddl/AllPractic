e705588cdc4733f61ec981d0b6e7223bfddc4e23
krenddl <wf.gabit@mail.ru>
2025-04-16 19:48:20 +0300

    budilnik

---
+++ PracticBudilnik/Budilnik/Views/BudilnikPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Budilnik.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для BudilnikPage.xaml
+    /// </summary>
+    public partial class BudilnikPage : Page
+    {
+        public BudilnikPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Budilnik_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new BuduilPage());
+        }
+
+        private void TimerBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PracticBudilnik/Budilnik/Views/BuduilPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Budilnik.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для BuduilPage.xaml
+    /// </summary>
+    public partial class BuduilPage : Page
+    {
+        private DispatcherTimer timerbudilnik;
+        private TimeSpan alarmbudilnik;
+        private bool Setalarmbudilnik;
+
+        public BuduilPage()
+        {
+            
+            InitializeComponent();
+            for (int i = 0; i < 24; i++)
+            {
+                HoursCb.Items.Add(i.ToString("00"));
+            }
+            for (int i = 0; i < 60; i++)
+            {
+                MinutesCb.Items.Add(i.ToString("00"));
+            }
+            HoursCb.SelectedIndex = DateTime.Now.Hour;
+            MinutesCb.SelectedIndex = DateTime.Now.Minute;
+
+            timerbudilnik = new DispatcherTimer();
+            timerbudilnik.Interval = TimeSpan.FromSeconds(1);
+
+
+        }
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
